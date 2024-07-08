6d4d6ff3c3c1f1fa6a91d7215e66d8fe568e219a
root <root@localhost>
2024-07-08 21:40:19 +0800

    update: 获取今日天气API

--- index.js
+++ index.js
@@ -1,71 +1,39 @@
 // 仿安卓Toast提示
 function toast(msg, duration) {
-  duration = isNaN(duration) ? 3000 : duration;
-  var m = document.createElement("div");
-  m.innerHTML = msg;
-  m.style.cssText =
-    "max-width: 70%;color: rgb(255, 255, 255);text-align: center;border-radius: 6px;padding: 4px 6px;position: fixed;top: 75%;left: 50%;transform: translate(-50%, 0);z-index: 99999999;background: rgba(0, 0, 0, 0.75);font-size: 14px;";
-  document.body.appendChild(m);
-  setTimeout(function () {
-    var d = 0.5;
-    m.style.webkitTransition =
-      "-webkit-transform " + d + "s ease-in, opacity " + d + "s ease-in";
-    m.style.opacity = "0";
-    setTimeout(function () {
-      document.body.removeChild(m);
-    }, d * 1000);
-  }, duration);
+    duration = isNaN(duration) ? 3000 : duration;
+    var m = document.createElement("div");
+    m.innerHTML = msg;
+    m.style.cssText =
+        "max-width: 70%;color: rgb(255, 255, 255);text-align: center;border-radius: 6px;padding: 4px 6px;position: fixed;top: 75%;left: 50%;transform: translate(-50%, 0);z-index: 99999999;background: rgba(0, 0, 0, 0.75);font-size: 14px;";
+    document.body.appendChild(m);
+    setTimeout(function() {
+        var d = 0.5;
+        m.style.webkitTransition =
+            "-webkit-transform " + d + "s ease-in, opacity " + d + "s ease-in";
+        m.style.opacity = "0";
+        setTimeout(function() {
+            document.body.removeChild(m);
+        }, d * 1000);
+    }, duration);
 }
 
 // 发送请求
-const xhr0 = new XMLHttpRequest();
+const xhr = new XMLHttpRequest();
 
-// 定位
-// 未完成
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(onSuccess, onError);
-} else {
-  toast("您的浏览器不支持使用HTML5来获取地理位置服务");
-}
-
-function onSuccess(position) {
-  var latitude = position.coords.latitude;
-  var longitude = position.coords.longitude;
-  xhr0.open(
-    "GET",
-    "https://api.map.baidu.com/geocoder?location=" +
-      latitude +
-      "," +
-      longitude +
-      "&output=json"
-  );
-  xhr0.onload = function () {
-    if (xhr0.status == 200) {
-      var response = xhr0.response;
-      toast(response);
-    } else {
-      toast(123);
+// 获取实时天气
+function getToday() {
+  xhr.open('GET', 'http://pitaya.tianqiapis.com/?version=today&unit=m&language=zh&appid=test&appsecret=test888');
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var today = xhr.responseText;
     }
   };
-  xhr0.send();
+  xhr.send();
 }
 
-function onError(error) {
-  switch (error.code) {
-    case error.PERMISSION_DENIED:
-      toast("您拒绝了对获取地理位置的请求");
-      break;
-    case error.POSITION_UNAVAILABLE:
-      toast("定位失败，请打开位置服务");
-      break;
-    case error.TIMEOUT:
-      toast("请求您的地理位置超时");
-      break;
-    case error.UNKNOWN_ERROR:
-      toast("未知错误");
-      break;
-  }
-}
+getToday();
+ 
+
 
 // 获取星期
 var week = "周" + "日一二三四五六".charAt(new Date().getDay());
