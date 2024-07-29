3a73b8ed7bc7600e27b71d2aa11656845ab01bac
yikang6 <2749436915@qq.com>
2024-07-30 00:12:23 +0800

    update: 优化代码

--- js/index.js
+++ js/index.js
@@ -1,21 +1,21 @@
 // 仿安卓Toast提示
 function toast(msg, duration) {
-    duration = isNaN(duration) ? 3000 : duration;
-    var m = document.createElement("div");
-    m.setAttribute('id', 'toast');
-    m.innerHTML = msg;
-    m.style.cssText =
-        "max-width: 70%;color: rgb(255, 255, 255);text-align: center;border-radius: 6px;padding: 4px 6px;position: fixed;top: 80%;left: 50%;transform: translate(-50%, 0);z-index: 99999999;background: rgba(0, 0, 0, 0.75);font-size: 14px;";
-    document.querySelector('html').appendChild(m);
+  duration = isNaN(duration) ? 3000 : duration;
+  var m = document.createElement("div");
+  m.setAttribute('id', 'toast');
+  m.innerHTML = msg;
+  m.style.cssText =
+    "max-width: 70%;color: rgb(255, 255, 255);text-align: center;border-radius: 6px;padding: 4px 6px;position: fixed;top: 80%;left: 50%;transform: translate(-50%, 0);z-index: 99999999;background: rgba(0, 0, 0, 0.75);font-size: 14px;";
+  document.querySelector('html').appendChild(m);
+  setTimeout(function() {
+    var d = 0.5;
+    m.style.webkitTransition =
+      "-webkit-transform " + d + "s ease-in, opacity " + d + "s ease-in";
+    m.style.opacity = "0";
     setTimeout(function() {
-        var d = 0.5;
-        m.style.webkitTransition =
-            "-webkit-transform " + d + "s ease-in, opacity " + d + "s ease-in";
-        m.style.opacity = "0";
-        setTimeout(function() {
-            document.querySelector('html').removeChild(m);
-        }, d * 1000);
-    }, duration);
+      document.querySelector('html').removeChild(m);
+    }, d * 1000);
+  }, duration);
 };
 
 // 发送请求
@@ -35,7 +35,7 @@ function sendGetRequest(url, callback) {
 function getWeather() {
   var provider = 'caiyun';
   var url = './js/api/' + provider + '.js';
-  
+
   var script = document.createElement('script');
   script.setAttribute('src', url);
   document.querySelector('body').appendChild(script);
@@ -44,39 +44,6 @@ function getWeather() {
 // DOM操作
 function dom(query, text) {
   document.querySelector(query).innerHTML = text;
-};
-
-// 定位
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(onSuccess, onError);
-} else {
-  toast('您的浏览器不支持使用HTML5来获取地理位置服务');
-};
-
-function onSuccess(position) {
-  latitude = position.coords.latitude;
-  longitude = position.coords.longitude;
-  
-  getWeather();
-};
-
-function onError(error) {
-  switch (error.code) {
-    case error.PERMISSION_DENIED:
-      toast('定位失败，请允许定位请求以获得更加精准的天气信息');
-      break;
-    case error.POSITION_UNAVAILABLE:
-      toast('定位失败，请打开位置服务以获得更加精准的天气信息');
-      break;
-    case error.TIMEOUT:
-      toast('请求您的地理位置超时，请重试');
-      break;
-    case error.UNKNOWN_ERROR:
-      toast('定位出现未知错误');
-      break;
-  };
-  
-  
 };
 
 // 获取星期
---
+++ js/locate.js
@@ -0,0 +1,70 @@
+/*
+  定位JS
+  优先使用设备硬件获取到的经纬度定位，失败则使用IP定位
+*/
+
+// 获取城市函数
+function getCity() {
+  time = Date.now();
+  
+  sendGetRequest(`https://api.tianditu.gov.cn/geocoder?postStr={'lon':` + longitude + `,'lat':` + latitude + `,'ver':1}&type=geocode&tk=922317ec85e9364922d3e97f06ab54fc`, function(response) {
+    var address = JSON.parse(response).result.addressComponent;
+
+    if (address.county == '') {
+      city = address.city;
+    } else {
+      city = address.county;
+    };
+
+    localStorage.setItem('city', city);
+    localStorage.setItem('time', time);
+    dom('.city', city);
+  });
+};
+
+// 通过Geolocation API获取经纬度
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(onSuccess, onError);
+} else {
+  toast('浏览器不支持GPS定位，建议更换浏览器以获得更加精准的天气信息');
+};
+
+function onSuccess(position) {
+  latitude = position.coords.latitude;
+  longitude = position.coords.longitude;
+
+  // 本地存储城市信息，减少API请求
+  if (localStorage.getItem('city') === null) {
+    getCity();
+  } else {
+    if (time - localStorage.getItem('time') >= 1800000) { // 数据超过30分钟自动刷新
+      getCity();
+    } else {
+      city = localStorage.getItem('city');
+      dom('.city', city);
+    };
+  };
+};
+
+// 失败后通过IP获取经纬度
+function onError(error) {
+  switch (error.code) {
+    case error.PERMISSION_DENIED:
+      toast('定位失败，请允许定位请求以获得更加精准的天气信息');
+      break;
+    case error.POSITION_UNAVAILABLE:
+      toast('定位失败，请打开位置服务以获得更加精准的天气信息');
+      break;
+    case error.TIMEOUT:
+      toast('请求您的地理位置超时，请重试');
+      break;
+    case error.UNKNOWN_ERROR:
+      toast('定位出现未知错误');
+      break;
+  };
+};
+
+setTimeout(function() {
+  document.querySelector('.start').remove();
+  document.querySelector('#app').style.opacity = '1';
+}, 1500);
