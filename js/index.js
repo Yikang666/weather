edc7a88482af782a7e34a29d772f54150d5646dd
Yikang666 <2749436915@qq.com>
2024-08-03 18:37:56 +0800

    update: 更新定位逻辑

--- js/index.js
+++ js/index.js
@@ -2,54 +2,74 @@
 function toast(msg, duration) {
   duration = isNaN(duration) ? 3000 : duration;
   var m = document.createElement("div");
-  m.setAttribute('id', 'toast');
+  m.setAttribute("id", "toast");
   m.innerHTML = msg;
   m.style.cssText =
     "max-width: 70%;color: rgb(255, 255, 255);text-align: center;border-radius: 6px;padding: 4px 6px;position: fixed;top: 80%;left: 50%;transform: translate(-50%, 0);z-index: 99999999;background: rgba(0, 0, 0, 0.75);font-size: 14px;";
-  document.querySelector('html').appendChild(m);
-  setTimeout(function() {
+  document.querySelector("html").appendChild(m);
+  setTimeout(function () {
     var d = 0.5;
-    m.style.webkitTransition =
+    m.style.transition =
       "-webkit-transform " + d + "s ease-in, opacity " + d + "s ease-in";
     m.style.opacity = "0";
-    setTimeout(function() {
-      document.querySelector('html').removeChild(m);
+    setTimeout(function () {
+      document.querySelector("html").removeChild(m);
     }, d * 1000);
   }, duration);
-};
+}
 
 // 发送请求
 // XML 普通GET请求
 const xhr = new XMLHttpRequest();
 
 function sendGetRequest(url, callback) {
-  xhr.open('GET', url);
-  xhr.onreadystatechange = function() {
+  xhr.open("GET", url);
+  xhr.onreadystatechange = function () {
     if (xhr.readyState == 4 && xhr.status == 200) {
       callback(xhr.response);
     }
   };
   xhr.send();
-};
+}
+
+// 当前时间戳
+time = Date.now();
+
+// 本地存储城市信息，减少API请求
+if (localStorage.getItem("city") === null) {
+  importJS("./js/locate.js");
+} else {
+  if (time - localStorage.getItem("time") >= 1800000) {
+    importJS("./js/locate.js");
+  } else {
+    city = localStorage.getItem("city");
+    dom(".city", city);
+  }
+}
+
+// 引入JS文件函数
+function importJS(url) {
+  var script = document.createElement("script");
+  script.src = url;
+  document.querySelector("body").appendChild(script);
+}
 
 // 引入数据源获取天气
 function getWeather() {
-  var provider = 'caiyun';
-  var url = './js/api/' + provider + '.js';
-
-  var script = document.createElement('script');
-  script.setAttribute('src', url);
-  document.querySelector('body').appendChild(script);
-};
+  var provider = "caiyun";
+  importJS("./js/api/" + provider + ".js");
+}
 
 // DOM操作
 function dom(query, text) {
   document.querySelector(query).innerHTML = text;
-};
+}
 
 // 获取星期
 var week = "周" + "日一二三四五六".charAt(new Date().getDay());
 dom(".a1 .day", week);
 
-// 当前时间戳
-time = Date.now();
+setTimeout(function () {
+  document.querySelector(".start").remove();
+  document.querySelector("#app").style.opacity = "1";
+}, 2000);
--- js/locate.js
+++ js/locate.js
@@ -5,88 +5,74 @@
 
 // 获取城市函数
 function getCity() {
-  sendGetRequest(`https://api.tianditu.gov.cn/geocoder?postStr={'lon':` + longitude + `,'lat':` + latitude + `,'ver':1}&type=geocode&tk=922317ec85e9364922d3e97f06ab54fc`, function(response) {
-    var address = JSON.parse(response).result.addressComponent;
+  sendGetRequest(
+    `https://api.tianditu.gov.cn/geocoder?postStr={'lon':` +
+      longitude +
+      `,'lat':` +
+      latitude +
+      `,'ver':1}&type=geocode&tk=922317ec85e9364922d3e97f06ab54fc`,
+    function (response) {
+      var address = JSON.parse(response).result.addressComponent;
 
-    if (address.county == '') {
-      city = address.city;
-    } else {
-      city = address.county;
-    };
+      if (address.county == "") {
+        city = address.city;
+      } else {
+        city = address.county;
+      }
 
-    localStorage.setItem('city', city);
-    localStorage.setItem('time', Date.now());
-    dom('.city', city);
-  });
-};
+      localStorage.setItem("city", city);
+      localStorage.setItem("time", Date.now());
+      dom(".city", city);
+    }
+  );
+}
 
 function getCityByIP() {
-  sendGetRequest('https://api.ipify.cn/', function(response) {
+  sendGetRequest("https://api.ipify.cn/", function (response) {
     var ip = response;
-    sendGetRequest('https://api.weather.888-114514.eu.org/ip?ip=' + ip, function(response) {
-      city = (JSON.parse(response).regions.pop());
+    sendGetRequest(
+      "https://api.weather.888-114514.eu.org/ip?ip=" + ip,
+      function (response) {
+        city = JSON.parse(response).regions.pop();
 
-      localStorage.setItem('city', city);
-      localStorage.setItem('time', Date.now());
-      dom('.city', city);
-    });
+        localStorage.setItem("city", city);
+        localStorage.setItem("time", Date.now());
+        dom(".city", city);
+      }
+    );
   });
-};
+}
 
 // 通过Geolocation API获取经纬度
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(onSuccess, onError);
 } else {
-  toast('浏览器不支持GPS定位，建议更换浏览器以获得更加精准的天气信息');
-};
+  toast("浏览器不支持GPS定位，建议更换浏览器以获得更加精准的天气信息");
+}
 
 function onSuccess(position) {
   latitude = position.coords.latitude;
   longitude = position.coords.longitude;
 
-  // 本地存储城市信息，减少API请求
-  if (localStorage.getItem('city') === null) {
-    getCity();
-  } else {
-    if (time - localStorage.getItem('time') >= 1800000) { // 数据超过30分钟自动刷新
-      getCity();
-    } else {
-      city = localStorage.getItem('city');
-      dom('.city', city);
-    };
-  };
-};
+  getCity();
+}
 
 // 失败后通过IP获取经纬度
 function onError(error) {
   switch (error.code) {
     case error.PERMISSION_DENIED:
-      toast('定位失败，请允许定位请求以获得更加精准的天气信息');
+      toast("定位失败，请允许定位请求以获得更加精准的天气信息");
       break;
     case error.POSITION_UNAVAILABLE:
-      toast('定位失败，请打开位置服务以获得更加精准的天气信息');
+      toast("定位失败，请打开位置服务以获得更加精准的天气信息");
       break;
     case error.TIMEOUT:
-      toast('请求您的地理位置超时，请重试');
+      toast("请求您的地理位置超时，请重试");
       break;
     case error.UNKNOWN_ERROR:
-      toast('定位出现未知错误');
+      toast("定位出现未知错误");
       break;
-  };
+  }
 
-  if (localStorage.getItem('city') === null) {
-    getCityByIP();
-  } else {
-    if (time - localStorage.getItem('time') >= 1800000) {
-      getCityByIP();
-    } else {
-      city = localStorage.getItem('city');
-      dom('.city', city);
-    };
-  };
-};
-
-setTimeout(function() {
-  document.querySelector('.start').remove();
-  document.querySelector('#app').style.opacity = '1';
-}, 2000);
+  getCityByIP();
+}
