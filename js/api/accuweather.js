f1eac099bc7852bf6e26e899ce8d70dc51a52470
Yikang666 <2749436915@qq.com>
2024-08-05 16:39:14 +0800

    update: 优化代码

--- js/api/accuweather.js
+++ js/api/accuweather.js
@@ -1,9 +1,17 @@
-var apiUrl = 'https://api.weather.888-114514.eu.org';
-  
-sendGetRequest(apiUrl + '/location/' + latitude.toFixed(2).replace('.', '') + '_' + longitude.toFixed(2).replace('.', '') + '_zh-cn.json.gz', function(response) {
-  var city = JSON.parse(response).cities[0];
-  cityId = city.lk;
-  cityName = city.ln;
-  
-  dom('.city', cityName);
-});
+var apiUrl = "https://api.weather.888-114514.eu.org";
+
+sendGetRequest(
+  apiUrl +
+    "/location/" +
+    latitude.toFixed(2).replace(".", "") +
+    "_" +
+    longitude.toFixed(2).replace(".", "") +
+    "_zh-cn.json.gz",
+  function (response) {
+    var city = JSON.parse(response).cities[0];
+    cityId = city.lk;
+    cityName = city.ln;
+
+    dom(".city", cityName);
+  }
+);
